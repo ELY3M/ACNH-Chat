fd99376535b0d620126217e8af7502666379a248
ELY M <elymbmx@gmail.com>
2022-08-13 13:47:27 -0500

    first commit

---
+++ MainActivity.cs
@@ -0,0 +1,453 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using System.Net.Sockets;
+using Environment = System.Environment;
+using System.Text;
+using Java.Lang;
+using String = System.String;
+using Exception = System.Exception;
+using System.Text.RegularExpressions;
+using Android.Content;
+using Android.Preferences;
+using System.Net;
+
+namespace ACNH_Chat
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+
+
+    public class MainActivity : AppCompatActivity
+    {
+
+
+
+        private Socket socket;
+        private static string chat = "[main+4AA9CD8]+40";
+        private static bool sendLock = false;
+
+
+        private ISharedPreferences sp;
+        private ISharedPreferencesEditor spedit;
+        String getipaddress = "";
+        bool connecting = false;
+        bool chatting = false;
+
+        private controller controller = null;
+
+        AppCompatEditText ipaddresstext;
+        AppCompatButton connectbutton;
+
+        AppCompatButton startchat;
+        AppCompatEditText chattext;
+        AppCompatButton sendbutton;
+
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+
+            var policy = new StrictMode.ThreadPolicy.Builder().PermitAll().Build();
+            StrictMode.SetThreadPolicy(policy);
+            
+
+
+            sp = PreferenceManager.GetDefaultSharedPreferences(Application.Context);
+            spedit = sp.Edit();
+
+            ipaddresstext = FindViewById<AppCompatEditText>(Resource.Id.ipaddress);
+            connectbutton = FindViewById<AppCompatButton>(Resource.Id.connect);
+
+            connectbutton.Click += (sender, e) =>
+            {
+
+                spedit.PutString("ipaddress", ipaddresstext.Text.ToString());
+                spedit.Commit();
+
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipaddresstext.Text), 6000);
+
+                IAsyncResult result = socket.BeginConnect(ep, null, null);
+                bool conSuceded = result.AsyncWaitHandle.WaitOne(3000, true);
+                if (conSuceded == true)
+                {
+                    connectbutton.SetBackgroundColor(Android.Graphics.Color.Lime);
+                    connectbutton.Text = "Connected";
+                    controller = new controller(socket);
+                }
+                else {
+                    connectbutton.SetBackgroundColor(Android.Graphics.Color.Aqua);
+                    connectbutton.Text = "Connect";
+                }
+                };
+
+
+
+            chattext = FindViewById<AppCompatEditText>(Resource.Id.chat);
+            startchat = FindViewById<AppCompatButton>(Resource.Id.startchat);
+
+            getipaddress = sp.GetString("ipaddress", "");
+            ipaddresstext.Text = getipaddress;
+
+
+
+            sendbutton = FindViewById<AppCompatButton>(Resource.Id.send);
+
+            startchat.Click += (sender, e) =>
+            {
+                controller.detachController();
+                controller.clickA();
+                controller.clickZR();
+                Thread.Sleep(1000);
+                controller.clickB();
+                controller.clickB();
+                controller.clickB();
+
+            };
+
+
+
+            sendbutton.Click += (sender, e) => {
+
+                Chat(socket);
+                string cleanStr = chattext.Text.Trim().Replace("\n", " ");
+
+                if (sendLock)
+                    return;
+                if (cleanStr.Equals(""))
+                    return;
+
+
+                sendLock = true;
+
+                Thread sendThread = new Thread(delegate () { sendChat(cleanStr); });
+                sendThread.Start();
+
+
+
+            };
+
+
+
+
+        }
+
+
+
+        
+
+        public static void SendString(Socket socket, byte[] buffer, int offset = 0, int size = 0, int timeout = 100)
+        {
+            int startTickCount = Environment.TickCount;
+            int sent = 0;  // how many bytes is already sent
+            if (size == 0)
+                for (int i = offset; i < buffer.Length; i++)
+                    if (buffer[i] == 0xA)
+                    {
+                        size = i + 1 - offset;
+                        break;
+                    }
+            if (size == 0) size = buffer.Length - offset;
+            do
+            {
+                if (Environment.TickCount > startTickCount + timeout)
+                    throw new Exception("Timeout.");
+                try
+                {
+                    sent += socket.Send(buffer, offset + sent, size - sent, SocketFlags.None);
+                }
+                catch (SocketException ex)
+                {
+                    if (ex.SocketErrorCode == SocketError.WouldBlock ||
+                        ex.SocketErrorCode == SocketError.IOPending ||
+                        ex.SocketErrorCode == SocketError.NoBufferSpaceAvailable)
+                    {
+                        // socket buffer is probably full, wait and try again
+                        //Thread.Sleep(10);
+                    }
+                    else
+                        throw;  // any serious error occurr
+                }
+            } while (sent < size);
+        }
+        public static int ReceiveString(Socket socket, byte[] buffer, int offset = 0, int size = 0, int timeout = 30000)
+        {
+            int startTickCount = Environment.TickCount;
+            int received = 0;  // how many bytes is already received
+            if (size == 0) size = buffer.Length - offset;
+            do
+            {
+                if (Environment.TickCount > startTickCount + timeout)
+                    throw new Exception("Timeout.");
+                try
+                {
+                    received += socket.Receive(buffer, offset + received, size - received, SocketFlags.None);
+                }
+                catch (SocketException ex)
+                {
+                    if (ex.SocketErrorCode == SocketError.WouldBlock ||
+                        ex.SocketErrorCode == SocketError.IOPending ||
+                        ex.SocketErrorCode == SocketError.NoBufferSpaceAvailable)
+                    {
+                        // socket buffer is probably empty, wait and try again
+                        //Thread.Sleep(30);
+                    }
+                    else
+                        throw;  // any serious error occurr
+                }
+            } while (received < size && buffer[received - 1] != 0xA);
+            return received;
+        }
+
+
+        public static void pokeAbsoluteAddress(Socket socket, string address, string value)
+        {
+            //lock (botLock)
+            {
+                string msg = String.Format("pokeAbsolute 0x{0:X8} 0x{1}\r\n", address, value);
+                SendString(socket, Encoding.UTF8.GetBytes(msg));
+            }
+        }
+
+
+        public static string ByteToHexString(byte[] b)
+        {
+            String hexString = BitConverter.ToString(b);
+            hexString = hexString.Replace("-", "");
+
+            return hexString;
+        }
+
+        public static byte[] ByteTrim(byte[] input)
+        {
+            int newLength = 1;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 0x0)
+                {
+                    newLength = i;
+                    break;
+                }
+            }
+
+            byte[] newArray = new byte[newLength];
+            Array.Copy(input, newArray, newArray.Length);
+
+            return newArray;
+        }
+
+        public static byte[] peekMainAddress(Socket socket, string address, int size)
+        {
+            //lock (botLock)
+            {
+                byte[] result = new byte[size];
+
+                string msg = String.Format("peekMain 0x{0:X8} 0x{1}\r\n", address, size);
+                //Debug.Print("PeekMain : " + msg);
+                SendString(socket, Encoding.UTF8.GetBytes(msg));
+
+                byte[] b = new byte[size * 2 + 64];
+                int first_rec = ReceiveString(socket, b);
+                string buffer = Encoding.ASCII.GetString(b, 0, size * 2);
+
+                if (buffer == null)
+                {
+                    return null;
+                }
+                for (int i = 0; i < size; i++)
+                {
+                    result[i] = Convert.ToByte(buffer.Substring(i * 2, 2), 16);
+                }
+
+                return result;
+            }
+        }
+
+        public static byte[] peekAbsoluteAddress(Socket socket, string address, int size)
+        {
+            //lock (botLock)
+            {
+                byte[] result = new byte[size];
+
+                string msg = String.Format("peekAbsolute 0x{0:X8} {1}\r\n", address, size);
+                SendString(socket, Encoding.UTF8.GetBytes(msg));
+                byte[] b = new byte[size * 2 + 64];
+                int first_rec = ReceiveString(socket, b);
+                string buffer = Encoding.ASCII.GetString(b, 0, size * 2);
+
+                if (buffer == null)
+                {
+                    return null;
+                }
+                for (int i = 0; i < size; i++)
+                {
+                    result[i] = Convert.ToByte(buffer.Substring(i * 2, 2), 16);
+                }
+
+                return result;
+            }
+        }
+
+        /*
+        public static void pokeAbsoluteAddress(Socket socket, string address, string value)
+        {
+            //lock (botLock)
+            {
+                string msg = String.Format("pokeAbsolute 0x{0:X8} 0x{1}\r\n", address, value);
+                SendString(socket, Encoding.UTF8.GetBytes(msg));
+            }
+        }
+        */
+        public static ulong GetCoordinateAddress(string strInput, Socket s)
+        {
+            //lock (lockObject)
+            {
+                // Regex pattern to get operators and offsets from pointer expression.	
+                string pattern = @"(\+|\-)([A-Fa-f0-9]+)";
+                Regex regex = new Regex(pattern);
+                Match match = regex.Match(strInput);
+
+                // Get first offset from pointer expression and read address at that offset from main start.	
+                var ofs = Convert.ToUInt64(match.Groups[2].Value, 16);
+                var address = BitConverter.ToUInt64(peekMainAddress(s, ofs.ToString("X"), 0x8), 0);
+                match = match.NextMatch();
+
+                // Matches the rest of the operators and offsets in the pointer expression.	
+                while (match.Success)
+                {
+                    // Get operator and offset from match.	
+                    string opp = match.Groups[1].Value;
+                    ofs = Convert.ToUInt64(match.Groups[2].Value, 16);
+
+                    // Add or subtract the offset from the current stored address based on operator in front of offset.	
+                    switch (opp)
+                    {
+                        case "+":
+                            address += ofs;
+                            break;
+                        case "-":
+                            address -= ofs;
+                            break;
+                    }
+
+                    // Attempt another match and if successful read bytes at address and store the new address.	
+                    match = match.NextMatch();
+                    if (match.Success)
+                    {
+                        byte[] bytes = peekAbsoluteAddress(s, address.ToString("X"), 0x8);
+                        address = BitConverter.ToUInt64(bytes, 0);
+                    }
+                }
+
+                return address;
+            }
+        }
+
+
+        
+        public void Chat(Socket Socket)
+        {
+            socket = Socket;
+
+            ///InitializeComponent();
+            chattext.SelectAll();
+
+        }
+        
+
+        private void chatButton_Click(object sender, EventArgs e)
+        {
+            string cleanStr = chattext.Text.Trim().Replace("\n", " ");
+
+            if (sendLock)
+                return;
+            if (cleanStr.Equals(""))
+                return;
+
+
+            sendLock = true;
+
+            Thread sendThread = new Thread(delegate () { sendChat(cleanStr); });
+            sendThread.Start();
+        }
+
+        private void sendChat(string message)
+        {
+            ulong ChatAddress = GetCoordinateAddress(chat, socket);
+
+            controller.clickR();
+            Thread.Sleep(800);
+            controller.clickY();
+
+            byte[] StrBytes = Encoding.Unicode.GetBytes(message);
+            byte[] sendBytes = new byte[StrBytes.Length * 2];
+            Buffer.BlockCopy(StrBytes, 0, sendBytes, 0, StrBytes.Length);
+            pokeAbsoluteAddress(socket, ChatAddress.ToString("X"), ByteToHexString(sendBytes));
+
+            controller.clickPLUS();
+            Thread.Sleep(400);
+
+            controller.clickB();
+            Thread.Sleep(200);
+            controller.clickB();
+            Thread.Sleep(200);
+            controller.clickB();
+            Thread.Sleep(200);
+
+            sendLock = false;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
---
+++ controller.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace ACNH_Chat
+{
+    class controller
+    {
+        private static Socket s;
+
+        private static readonly Encoding Encoder = Encoding.UTF8;
+        private static byte[] Encode(string command, bool addrn = true) => Encoder.GetBytes(addrn ? command + "\r\n" : command);
+        private static byte[] X() => Encode("click X");
+        private static byte[] pX() => Encode("press X");
+        private static byte[] rX() => Encode("release X");
+        private static byte[] Y() => Encode("click Y");
+        private static byte[] pY() => Encode("press Y");
+        private static byte[] rY() => Encode("release Y");
+        private static byte[] A() => Encode("click A");
+        private static byte[] pA() => Encode("press A");
+        private static byte[] rA() => Encode("release A");
+        private static byte[] B() => Encode("click B");
+        private static byte[] pB() => Encode("press B");
+        private static byte[] rB() => Encode("release B");
+
+        private static byte[] L() => Encode("click L");
+        private static byte[] R() => Encode("click R");
+        private static byte[] ZL() => Encode("click ZL");
+        private static byte[] ZR() => Encode("click ZR");
+
+        private static byte[] PLUS() => Encode("click PLUS");
+        private static byte[] MINUS() => Encode("click MINUS");
+
+        private static byte[] Home() => Encode("click HOME");
+        private static byte[] Capture() => Encode("click CAPTURE");
+
+        private static byte[] Up() => Encode("click DUP");
+        private static byte[] Right() => Encode("click DRIGHT");
+        private static byte[] Down() => Encode("click DDOWN");
+        private static byte[] Left() => Encode("click DLEFT");
+
+        private static byte[] LSTICK() => Encode("click LSTICK");
+        private static byte[] RSTICK() => Encode("click RSTICK");
+
+
+        private static byte[] pL() => Encode("press L");
+        private static byte[] rL() => Encode("release L");
+        private static byte[] detach() => Encode("detachController");
+
+        private static byte[] LstickTR() => Encode("setStick LEFT 0x7FFF 0x7FFF");
+        private static byte[] LstickTL() => Encode("setStick LEFT -0x8000 0x7FFF");
+        private static byte[] LstickBR() => Encode("setStick LEFT 0x7FFF -0x8000");
+        private static byte[] LstickBL() => Encode("setStick LEFT -0x8000 -0x8000");
+        private static byte[] LstickU() => Encode("setStick LEFT 0x0 0x7FFF");
+        private static byte[] LstickL() => Encode("setStick LEFT -0x8000 0x0");
+        private static byte[] LstickD() => Encode("setStick LEFT 0x0 -0x8000");
+        private static byte[] LstickR() => Encode("setStick LEFT 0x7FFF 0x0");
+        private static byte[] resetLeft() => Encode("setStick LEFT 0 0");
+        private static byte[] RstickU() => Encode("setStick RIGHT 0x0 0x7FFF");
+        private static byte[] RstickL() => Encode("setStick RIGHT -0x8000 0x0");
+        private static byte[] RstickD() => Encode("setStick RIGHT 0x0 -0x8000");
+        private static byte[] RstickR() => Encode("setStick RIGHT 0x7FFF 0x0");
+        private static byte[] resetRight() => Encode("setStick RIGHT 0 0");
+
+        private static string IslandName = "";
+
+        public controller(Socket S)
+        {
+            s = S;
+        }
+
+        public static void clickA()
+        {
+            SendString(s, A());
+        }
+
+        public static void pressA()
+        {
+            SendString(s, pA());
+        }
+
+        public static void releaseA()
+        {
+            SendString(s, rA());
+        }
+
+        public static void clickB()
+        {
+            SendString(s, B());
+        }
+
+        public static void pressB()
+        {
+            SendString(s, pB());
+        }
+
+        public static void releaseB()
+        {
+            SendString(s, rB());
+        }
+
+        public static void clickX()
+        {
+            SendString(s, X());
+        }
+
+        public static void pressX()
+        {
+            SendString(s, pX());
+        }
+
+        public static void releaseX()
+        {
+            SendString(s, rX());
+        }
+
+        public static void clickY()
+        {
+            SendString(s, Y());
+        }
+
+        public static void pressY()
+        {
+            SendString(s, pY());
+        }
+
+        public static void releaseY()
+        {
+            SendString(s, rY());
+        }
+
+        public static void clickL()
+        {
+            SendString(s, L());
+        }
+        public static void clickR()
+        {
+            SendString(s, R());
+        }
+        public static void clickZL()
+        {
+            SendString(s, ZL());
+        }
+        public static void clickZR()
+        {
+            SendString(s, ZR());
+        }
+
+        public static void clickPLUS()
+        {
+            SendString(s, PLUS());
+        }
+        public static void clickMINUS()
+        {
+            SendString(s, MINUS());
+        }
+
+        public static void clickHOME()
+        {
+            SendString(s, Home());
+        }
+        public static void clickCAPTURE()
+        {
+            SendString(s, Capture());
+        }
+
+        public static void clickUp()
+        {
+            SendString(s, Up());
+        }
+        public static void clickLeft()
+        {
+            SendString(s, Left());
+        }
+        public static void clickDown()
+        {
+            SendString(s, Down());
+        }
+        public static void clickRight()
+        {
+            SendString(s, Right());
+        }
+
+        public static void clickRightStick()
+        {
+            SendString(s, RSTICK());
+        }
+        public static void clickLeftStick()
+        {
+            SendString(s, LSTICK());
+        }
+
+        public static void pressL()
+        {
+            SendString(s, pL());
+        }
+        public static void releaseL()
+        {
+            SendString(s, rL());
+        }
+
+        public static void LstickTopRight()
+        {
+            SendString(s, LstickTR());
+        }
+        public static void LstickTopLeft()
+        {
+            SendString(s, LstickTL());
+        }
+        public static void LstickBottomRight()
+        {
+            SendString(s, LstickBR());
+        }
+        public static void LstickBottomLeft()
+        {
+            SendString(s, LstickBL());
+        }
+
+        public static void LstickUp()
+        {
+            SendString(s, LstickU());
+        }
+        public static void LstickLeft()
+        {
+            SendString(s, LstickL());
+        }
+        public static void LstickDown()
+        {
+            SendString(s, LstickD());
+        }
+        public static void LstickRight()
+        {
+            SendString(s, LstickR());
+        }
+        public static void resetLeftStick()
+        {
+            SendString(s, resetLeft());
+        }
+
+        public static void RstickUp()
+        {
+            SendString(s, RstickU());
+        }
+        public static void RstickLeft()
+        {
+            SendString(s, RstickL());
+        }
+        public static void RstickDown()
+        {
+            SendString(s, RstickD());
+        }
+        public static void RstickRight()
+        {
+            SendString(s, RstickR());
+        }
+        public static void resetRightStick()
+        {
+            SendString(s, resetRight());
+        }
+
+        public static void detachController()
+        {
+            SendString(s, detach());
+        }
+
+
+
+
+        public static void SendString(Socket socket, byte[] buffer, int offset = 0, int size = 0, int timeout = 100)
+        {
+            int startTickCount = Environment.TickCount;
+            int sent = 0;  // how many bytes is already sent
+            if (size == 0)
+                for (int i = offset; i < buffer.Length; i++)
+                    if (buffer[i] == 0xA)
+                    {
+                        size = i + 1 - offset;
+                        break;
+                    }
+            if (size == 0) size = buffer.Length - offset;
+            do
+            {
+                if (Environment.TickCount > startTickCount + timeout)
+                    throw new Exception("Timeout.");
+                try
+                {
+                    sent += socket.Send(buffer, offset + sent, size - sent, SocketFlags.None);
+                }
+                catch (SocketException ex)
+                {
+                    if (ex.SocketErrorCode == SocketError.WouldBlock ||
+                        ex.SocketErrorCode == SocketError.IOPending ||
+                        ex.SocketErrorCode == SocketError.NoBufferSpaceAvailable)
+                    {
+                        // socket buffer is probably full, wait and try again
+                        //Thread.Sleep(10);
+                    }
+                    else
+                        throw;  // any serious error occurr
+                }
+            } while (sent < size);
+        }
+
+
+
+
+
+    }
+}
